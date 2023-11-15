5a58807144865bffc62ba2bc5899751167de6b94
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-11-15 18:04:04 +0100

    ajout d'un template de base avec header, contenu et footer

---
+++ src/View/layout.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link href="./assets/css/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <header>
+            <div class="logo">NWS Boutique</div>
+            <nav>
+                <ul>
+                    <li><a href="index.php?page=accueil">Accueil</a></li>
+                    <li><a href="index.php?page=produits">Produits</a></li>
+                    <li><a href="">Panier</a></li>
+                    <li><a href="">Produits admin</a></li>
+                </ul>
+            </nav>
+        </header>
+        <?= $content ?>
+    </body>
+</html>
