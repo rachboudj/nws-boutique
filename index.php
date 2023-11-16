ccd9987ff2ea68a360f89413498a10dfb8b35e47
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-11-16 16:47:17 +0100

    ajout route vers inscription

--- index.php
+++ index.php
@@ -4,12 +4,15 @@ require_once "./src/Database/Database.php";
 
 require_once('src/Controller/accueil.php');
 require_once('src/Controller/produits.php');
+require_once('src/Controller/inscription.php');
 
 if (isset($_GET['page'])) {
     if ($_GET['page'] === 'accueil') {
         accueil();
     } elseif ($_GET['page'] === 'produits') {
         produits();
+    } elseif ($_GET['page'] === 'inscription') {
+        inscription();
     }
 } else {
     accueil();
