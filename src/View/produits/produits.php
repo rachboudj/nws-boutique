a8c49478d06a5b135e48cabd840df2622deb1048
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-11-17 15:32:52 +0100

    ajout du tableau HTML, affichage des données

--- src/View/produits/produits.php
+++ src/View/produits/produits.php
@@ -1,11 +1,41 @@
 <?php $title = "Les produits"; ?>
 
 <?php ob_start(); ?>
-<h1>Nos produits</h1>
 
-<?php
-echo $produits;
-?>
+<div class="container-table">
+    <h1>Nos produits projets</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Description</th>
+                <th>Image</th>
+                <th>Prix</th>
+                <th>Quantité</th>
+                <th>Mis en avant</th>
+                <th>Catégorie</th>
+                <th colspan="3">Opérations</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($produits as $produit) { ?>
+                <tr>
+                    <td><?= $produit['id_produit']; ?></td>
+                    <td><?= $produit['nom']; ?></td>
+                    <td><?= $produit['description']; ?></td>
+                    <td><?= $produit['image']; ?></td>
+                    <td><?= $produit['prix']; ?></td>
+                    <td><?= $produit['quantite']; ?></td>
+                    <td><?= $produit['misEnAvant']; ?></td>
+                    <td><?= $produit['id_categorie']; ?></td>
+                    <td><a href="index.php?page=modifProduit&amp;produitId=<?= $produit['id_produit'] ?>">Modifier</a></td>
+                    <td><a href="index.php?page=supprProduit&amp;produitId=<?= $produit['id_produit'] ?>">Supprimer</a></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
