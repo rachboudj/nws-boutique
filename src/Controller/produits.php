ad049ae606fcd1948b69c434b4ebd84b8a58e724
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-11-17 18:13:04 +0100

    ajout fonction pour gestion de la logique de modification et suppression

--- src/Controller/produits.php
+++ src/Controller/produits.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 
 require_once './src/Model/produits.php';
 require_once './src/utils/functions.php';
 
-function affichProduits() {
+function affichProduits()
+{
     $pdo = (new Database())->getPdo();
     $model = new ProduitsModel($pdo);
 
@@ -13,7 +14,8 @@ function affichProduits() {
     require './src/View/produits/produits.php';
 }
 
-function ajoutProduits() {
+function ajoutProduits()
+{
 
     $pdo = (new Database())->getPdo();
     $model = new ProduitsModel($pdo);
@@ -30,7 +32,7 @@ function ajoutProduits() {
         $errors = validationTexte($errors, $description, 'description', 3, 1000);
         $errors = validationTexte($errors, $quantite, 'quantite', 1, 1000);
         $errors = validationTexte($errors, $prix, 'prix', 1, 1000);
-    
+
         if (count($errors) === 0) {
             $model->addProducts($nom, $description, $image, $quantite, $prix);
             echo "Le produit a été enregistré avec succès !";
@@ -40,4 +42,55 @@ function ajoutProduits() {
     }
 
     require './src/View/produits/ajoutProduits.php';
+}
+
+
+function modifProduits()
+{
+
+
+    $pdo = (new Database())->getPdo();
+    $model = new ProduitsModel($pdo);
+
+    $errors = array();
+
+    if (!empty($_GET['produitId']) && ctype_digit($_GET['produitId'])) {
+        $id = $_GET['produitId'];
+
+        $produits = $model->showEditProducts($id);
+
+        if (!empty($_POST['submit'])) {
+            $nom = trim(strip_tags($_POST['nom']));
+            $description = trim(strip_tags($_POST['description']));
+            $image = trim(strip_tags($_POST['image']));
+            $quantite = trim($_POST['quantite']);
+            $prix = trim($_POST['prix']);
+
+            $errors = validationTexte($errors, $nom, 'nom', 3, 100);
+            $errors = validationTexte($errors, $description, 'description', 3, 1000);
+            $errors = validationTexte($errors, $quantite, 'quantite', 1, 1000);
+            $errors = validationTexte($errors, $prix, 'prix', 1, 1000);
+
+            if (count($errors) === 0) {
+                $model->editProducts($nom, $description, $image, $quantite, $prix, $id);
+                header('Location: index.php?page=produits');
+            }
+        }
+    }
+
+
+    require './src/View/produits/modifProduits.php';
+}
+
+function supprProduits()
+{
+    $pdo = (new Database())->getPdo();
+    $model = new ProduitsModel($pdo);
+
+    if (!empty($_GET['produitId']) && ctype_digit($_GET['produitId'])) {
+        $id = $_GET['produitId'];
+        $model->deleteProducts($id);
+        header('Location: index.php?page=produits');
+        exit();
+    }
 }
