646fe28498bbdf1a2969967a93b418f143d46796
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-11-17 18:29:15 +0100

    ajout affichage des données

--- src/View/accueil.php
+++ src/View/accueil.php
@@ -3,9 +3,18 @@
 <?php ob_start(); ?>
 <h1>Bienvenue sur NWS Boutique !</h1>
 
-<?php
-echo $accueil;
-?>
+<div class="container">
+    <div class="card">
+        <?php foreach ($produits as $produit) { ?>
+            <img src="<?= $produit['image']; ?>" alt="<?= $produit['nom']; ?>">
+            <h3><?= $produit['nom']; ?></h3>
+            <span><?= $produit['prix']; ?></span>
+            <button><a href="">Ajouter au panier</a></button>
+        <?php } ?>
+    </div>
+</div>
+
+
 
 <?php $content = ob_get_clean(); ?>
 
